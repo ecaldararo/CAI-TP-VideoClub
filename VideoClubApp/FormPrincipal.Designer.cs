7b0ed394e7a4b3ca9f11dbecf2f279ee2647ee90
Usuario <isoldimariano@gmail.com>
2021-05-26 21:41:48 -0300

    first commit

---
+++ VideoClubApp/FormPrincipal.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace VideoClubApp
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonPeliculas = new System.Windows.Forms.Button();
+            this.botonPrestamos = new System.Windows.Forms.Button();
+            this.botonClientes = new System.Windows.Forms.Button();
+            this.botonReportes = new System.Windows.Forms.Button();
+            this.panelIzquierdo = new System.Windows.Forms.Panel();
+            this.panelSuperior = new System.Windows.Forms.Panel();
+            this.panelInteraccion = new System.Windows.Forms.Panel();
+            this.tituloPrincipal = new System.Windows.Forms.Label();
+            this.panelIzquierdo.SuspendLayout();
+            this.panelSuperior.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // botonPeliculas
+            // 
+            this.botonPeliculas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botonPeliculas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonPeliculas.Location = new System.Drawing.Point(4, 66);
+            this.botonPeliculas.Margin = new System.Windows.Forms.Padding(4);
+            this.botonPeliculas.Name = "botonPeliculas";
+            this.botonPeliculas.Size = new System.Drawing.Size(212, 64);
+            this.botonPeliculas.TabIndex = 17;
+            this.botonPeliculas.Text = "Películas";
+            this.botonPeliculas.UseVisualStyleBackColor = false;
+            // 
+            // botonPrestamos
+            // 
+            this.botonPrestamos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonPrestamos.Location = new System.Drawing.Point(4, 135);
+            this.botonPrestamos.Margin = new System.Windows.Forms.Padding(4);
+            this.botonPrestamos.Name = "botonPrestamos";
+            this.botonPrestamos.Size = new System.Drawing.Size(212, 62);
+            this.botonPrestamos.TabIndex = 18;
+            this.botonPrestamos.Text = "Préstamos";
+            this.botonPrestamos.UseVisualStyleBackColor = true;
+            this.botonPrestamos.Click += new System.EventHandler(this.botonPrestamos_Click);
+            // 
+            // botonClientes
+            // 
+            this.botonClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonClientes.Location = new System.Drawing.Point(4, 205);
+            this.botonClientes.Margin = new System.Windows.Forms.Padding(4);
+            this.botonClientes.Name = "botonClientes";
+            this.botonClientes.Size = new System.Drawing.Size(212, 62);
+            this.botonClientes.TabIndex = 21;
+            this.botonClientes.Text = "Clientes";
+            this.botonClientes.UseVisualStyleBackColor = true;
+            // 
+            // botonReportes
+            // 
+            this.botonReportes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonReportes.Location = new System.Drawing.Point(4, 275);
+            this.botonReportes.Margin = new System.Windows.Forms.Padding(4);
+            this.botonReportes.Name = "botonReportes";
+            this.botonReportes.Size = new System.Drawing.Size(212, 62);
+            this.botonReportes.TabIndex = 20;
+            this.botonReportes.Text = "Reportes";
+            this.botonReportes.UseVisualStyleBackColor = true;
+            // 
+            // panelIzquierdo
+            // 
+            this.panelIzquierdo.Controls.Add(this.botonPrestamos);
+            this.panelIzquierdo.Controls.Add(this.botonPeliculas);
+            this.panelIzquierdo.Controls.Add(this.botonClientes);
+            this.panelIzquierdo.Controls.Add(this.botonReportes);
+            this.panelIzquierdo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.panelIzquierdo.Name = "panelIzquierdo";
+            this.panelIzquierdo.Size = new System.Drawing.Size(222, 705);
+            this.panelIzquierdo.TabIndex = 22;
+            // 
+            // panelSuperior
+            // 
+            this.panelSuperior.Controls.Add(this.tituloPrincipal);
+            this.panelSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSuperior.Location = new System.Drawing.Point(222, 0);
+            this.panelSuperior.Name = "panelSuperior";
+            this.panelSuperior.Size = new System.Drawing.Size(994, 69);
+            this.panelSuperior.TabIndex = 23;
+            // 
+            // panelInteraccion
+            // 
+            this.panelInteraccion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelInteraccion.Location = new System.Drawing.Point(222, 69);
+            this.panelInteraccion.Name = "panelInteraccion";
+            this.panelInteraccion.Size = new System.Drawing.Size(994, 636);
+            this.panelInteraccion.TabIndex = 24;
+            // 
+            // tituloPrincipal
+            // 
+            this.tituloPrincipal.AutoSize = true;
+            this.tituloPrincipal.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tituloPrincipal.Location = new System.Drawing.Point(392, 19);
+            this.tituloPrincipal.Name = "tituloPrincipal";
+            this.tituloPrincipal.Size = new System.Drawing.Size(174, 34);
+            this.tituloPrincipal.TabIndex = 0;
+            this.tituloPrincipal.Text = "Video Club";
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 705);
+            this.Controls.Add(this.panelInteraccion);
+            this.Controls.Add(this.panelSuperior);
+            this.Controls.Add(this.panelIzquierdo);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormPrincipal";
+            this.Text = "Form1";
+            this.panelIzquierdo.ResumeLayout(false);
+            this.panelSuperior.ResumeLayout(false);
+            this.panelSuperior.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonPeliculas;
+        private System.Windows.Forms.Button botonPrestamos;
+        private System.Windows.Forms.Button botonClientes;
+        private System.Windows.Forms.Button botonReportes;
+        private System.Windows.Forms.Panel panelIzquierdo;
+        private System.Windows.Forms.Panel panelSuperior;
+        private System.Windows.Forms.Panel panelInteraccion;
+        private System.Windows.Forms.Label tituloPrincipal;
+    }
+}
+
---
+++ VideoClubApp/Forms/FormClientes.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace VideoClubApp.Forms
+{
+    partial class FormClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 69);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Modificar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "DNI";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(38, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Agregar Cliente";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(206, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(634, 311);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(572, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Apellido";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(406, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Nombre";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(371, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(133, 22);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(535, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 22);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(706, 63);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(133, 22);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(742, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Apellido";
+            // 
+            // FormClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "FormClientes";
+            this.Text = "FormClientes";
+            this.Load += new System.EventHandler(this.FormClientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
