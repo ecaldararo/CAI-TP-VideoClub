7b501f21a82e7dfd0b686f915f6129c7036e79f7
ecaldararo <ecaldararo@gmail.com>
2021-05-26 21:33:53 -0300

    lio

---
+++ VideoClubApp/FormPrincipal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoClubApp
+{
+    public partial class FormPrincipal : Form
+    {
+        private Form formActivo;
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirFormInteractivo(Form formHijo, object botonSender)
+        {
+            if(formActivo != null)
+            {
+                formActivo.Close();
+            }
+            formActivo = formHijo;
+            formHijo.TopLevel = false;
+            formHijo.FormBorderStyle = FormBorderStyle.None;
+            formHijo.Dock = DockStyle.Fill;
+            this.panelInteraccion.Controls.Add(formHijo);
+            this.panelInteraccion.Tag = formHijo;
+            formHijo.BringToFront();
+            formHijo.Show();
+            tituloPrincipal.Text = formHijo.Text;
+        }
+
+        private void botonPrestamos_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- VideoClubApp/Forms/FormClientes.cs
+++ VideoClubApp/Forms/FormClientes.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace VideoClubApp
+namespace VideoClubApp.Forms
 {
-    public partial class Form1 : Form
+    public partial class FormPeliculas : Form
     {
-        public Form1()
+        public FormPeliculas()
         {
             InitializeComponent();
         }
